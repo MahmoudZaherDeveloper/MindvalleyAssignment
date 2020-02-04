308f419199d827cd2e2da8c552d47214f83a6483
MahmoudZaher <mahmoudzaher.developer@gmail.com>
2020-02-04 14:54:58 +0200

    initial commit

---
+++ urlloader/src/main/java/com/mindvalley/urlloader/DownloadableString.kt
@@ -0,0 +1,8 @@
+package com.mindvalley.urlloader
+
+class DownloadableString(url: String) : Downloadable<String>(url) {
+    //converts ByteArray to string
+    override fun transform(content: DownloadableContent): String {
+        return content.toString(Charsets.UTF_8)
+    }
+}
